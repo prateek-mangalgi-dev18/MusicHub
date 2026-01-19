74ddca5575a3d4b7ecdd28b8c40e4dc7c494762b
PrateekMangalgi <prateekmangalgi924@gmail.com>
2026-01-19 23:51:45 +0530

    added .env file

--- user/backend/src/index.js
+++ user/backend/src/index.js
@@ -12,7 +12,7 @@ const songRoutes = require("../routes/songroutes");
 const userRoutes = require("../routes/user");
 
 // -------------------- DB --------------------
-connectToMongoDB("mongodb://localhost:27017/rr")
+connectToMongoDB(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB error", err));
 
